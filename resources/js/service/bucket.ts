650c1d7e23b75371b9abb70f3df76625a976a17f
virus01064 <jp1jn04@gmail.com>
2025-04-01 19:26:45 +0800

    fix: changing await top level method

--- resources/js/service/bucket.ts
+++ resources/js/service/bucket.ts
@@ -1,9 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://rbuqmnhadlqtaxfebjzj.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY; // Ganti dengan kunci API dari Supabase
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY; 
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-let { data: users } = await supabase.from('users').select('role').single();
-export const userRole = users?.role || "guest";
-export default supabase ;
+
+let userRole = "guest"; // Default role
+
+(async () => {
+  let { data: users } = await supabase.from('users').select('role').single();
+  userRole = users?.role || "guest";
+})();
+
+export { supabase, userRole };
+export default supabase;
